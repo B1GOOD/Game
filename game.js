104049f5038b85ebf1da93e2d1c95e0f9e1f0df8
Renatinha_Anjos <rafael.gabrielc@oulook.com>
2024-06-15 12:48:07 -0300

    Adicionado lógica do jogo

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 let executando = false;
+let acertos = 0;
+
 
 const startBotao = document.querySelector("#btn--start");
 const numSorteado = document.querySelector(".num--atual");
@@ -6,12 +8,15 @@ const numSorteado = document.querySelector(".num--atual");
 // const botaoBaixo = document.querySelector("btn--baixo")
 // const start = document.getElementById("btn-start") 
 
+const placarVit = document.querySelector(".num--vit");
+const placarDev = document.querySelector(".num--der")
+
 startBotao.addEventListener("click", startGame)
 
 function startGame(){
 
     if (executando === true ){
-        alert("jogo já está em andamento... ✔")
+        // alert("jogo já está em andamento... ✔")
         return 
     } else {
         //sorteando numero de 1 á 20 
@@ -23,5 +28,36 @@ function startGame(){
 } 
 
 function verificarResposta(palpite){
-    alert(`O palpite foi ${palpite}`)
+    // alert(`O palpite foi ${palpite}`)
+    let numAnterior = Number(numSorteado.textContent);
+    numSorteado.textContent = Math.floor(Math.random()*20+1)
+    let numNovo = numSorteado.textContent;
+    // O if
+    if (palpite === "alto" && numNovo > numAnterior){
+        acertos++
+        verificarVitoria();
+    }else if (palpite === "baixo" && numNovo < numAnterior){
+        acertos++
+        verificarVitoria();
+    }else{
+        gameOver()
+    }
+} 
+
+function sortearNum() {
+    return numSorteado.textContent = Math.floor(Math.random())*20+1
+}
+
+function verificarVitoria(){
+    if (acertos >= 3){
+        placarVit.textContent++
+        acertos = 0;
+    }
+}
+
+function gameOver(){
+    placarDev.textContent++;
+    executando = false
+    numSorteado.textContent = "";
+    acertos = 0
 }
